6053fe09902574862bf20e381d328b378d686b9b
NikoRestifo <ndr45@georgetown.edu>
2021-02-03 18:13:33 -0500

    "If" statements

--- game.py
+++ game.py
@@ -20,9 +20,17 @@ print(f"The computer chose: {computer_choice}")
 
 #determining who won
 
-exit()
+if user_choice == computer_choice:
+    print("It is a tie!")
+elif user_choice == "rock" and computer_choice == "paper":
+    print("You lose!")
+elif user_choice == "scissors" and computer_choice == "rock":
+    print("You lose!")
+elif user_choice == "scissors" and computer_choice == "paper":
+    print("You win!")
+elif user_choice == "paper" and computer_choice == "rock":
+    print("You win!")
+elif user_choice == "rock" and computer_choice == "scissors":
+    print("You win!")
 
-print("-------------------")
-print("Oh, the computer won. It's ok.")
-print("-------------------")
 print("Thanks for playing. Please play again!")
